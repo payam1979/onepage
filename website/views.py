e28c522d7d1f520c4dd5182d8f3b2d708cce1383
Payam <bagheri.payam@gmail.com>
2024-02-29 20:46:19 +0330

    last test
    change in html file from django python view contest

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-     return render(request, 'index.html')
+     context={'years':'2000 - present', 'title':'Business Intelligence Developer'}
+     return render(request, 'index.html', context)
 
     
